0bbe6bfcaad96c2ee78f6aa2914176da4d9b696a
Nəriman Məmmədli <neriman.memmedli001@gmail.com>
2021-01-15 05:47:24 +0400

    msg test

--- src/components/Client.ts
+++ src/components/Client.ts
@@ -20,7 +20,9 @@ export default class Tweetcord extends Client {
         this.commands = new Collection()
     }
     private handleMessage(message: Message) {
-        if (!message.content.startsWith(this.config.prefix) || message.author.bot || message.webhookID || !(message.channel as TextChannel).permissionsFor(message.guild.me).has("SEND_MESSAGES")) return;
+      console.log("msg brrr")
+        const channel = message.channel as TextChannel;
+        if (!message.content.startsWith(this.config.prefix) || message.author.bot || message.webhookID || !channel.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return;
         const [prefix, ...args] = message.content.slice(this.config.prefix.length).trim().split(/ +g/)
         const command: Command = this.findCommand(args.shift())
         if (command) {
--- src/components/Logger.ts
+++ src/components/Logger.ts
@@ -1,8 +1,15 @@
 import { bgYellow, bgRed, bgGreen, bgBlue } from "chalk";
 export default class Logger {
     private log(m) {
-        const date = 
-        console.log(m)
+        const date = new Date(Date.now()).toLocaleString("az-AZ", {
+            timeZone: "Asia/Baku",
+            hour12: true,
+        })
+        console.log(`${date} ${m}`)
+    }
+
+    public info(input: any) {
+  return this.log(input)
     }
 
     public warn(input: any) {
