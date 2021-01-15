1bc7b474cc16f87153960b1fe2f482c5ff32b353
Nəriman Məmmədli <neriman.memmedli001@gmail.com>
2021-01-15 04:45:31 +0400

    fix

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,13 +1,15 @@
-import { Message } from "discord.js";
+import { Client, Message } from "discord.js";
 import Command from "../components/Command";
 
 export default class Ping extends Command {
-    constructor(client) {
+    client: Client;
+    constructor(client: Client) {
         super(client, {
             triggers: ["ping", "latency"]
-        })
+        });
+        this.client = client;
     }
-   public run(message: Message, args): Promise<Message> {
+   public run(message: Message, args: string[]): Promise<Message> {
        return message.reply(`${this.bot.ws.ping} ms.`)
    }
 }
