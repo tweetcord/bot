7a7fe612d2cbbf781281afa1b125ef7e1dce9f45
Nəriman Məmmədli <neriman.memmedli001@gmail.com>
2021-01-21 09:02:14 +0400

    use bodyparser

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
+import bodyparser from "body-parser"
 import * as config from "../Config"
 
 
 const app = express()
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(bodyparser.json())
 app.listen(8080, () => console.log("Started api in 8080"))
 
 
@@ -14,7 +16,7 @@ app.post("/vote", (req: Request, res: Response) => {
     }
     
     const body = JSON.parse(req.body)
-    console.log(req.body, body)
+    console.log(req.body)
     
     return res.status(200)
 })
