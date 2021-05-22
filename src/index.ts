36d9af2188e7b3a30d9e318cc8c6446dfa8f9d36
Nəriman Məmmədli <neriman.memmedli001@gmail.com>
2021-05-23 02:57:09 +0400

    express server

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
-import { SlashCreator, GatewayServer } from 'slash-create';
+import { SlashCreator, ExpressServer } from 'slash-create';
 import Discord from 'discord.js-light';
 const client = new Discord.Client();
-import path from 'path';
 const creator = new SlashCreator({
     applicationID: '696101246045519882',
     publicKey: 'e523d746fa9b3317ba7f5e0fbd37885dff130d0628327b27cd1da5cae64b5f3b',
     token: 'Njk2MTAxMjQ2MDQ1NTE5ODgy.Xoj0zQ.iP2MpAqmIGC_QlIjuxEz56OwoCU',
 });
 
-creator.withServer(new GatewayServer((handler) =>
-    client.on('interactionCreate', handler)))
-    .registerCommandsIn(path.join(__dirname, 'commands'))
-    .syncCommands();
+creator.withServer(new ExpressServer()).startServer();
 
 client.login('Njk2MTAxMjQ2MDQ1NTE5ODgy.Xoj0zQ.iP2MpAqmIGC_QlIjuxEz56OwoCU')
