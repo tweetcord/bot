da6bd0fa29d26cfafe9368de621bec2232fdd232
Neriman Memmedli <neriman.memmedli001@gmail.com>
2021-07-20 14:20:57 +0400

    tweet search command, closes #99

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction, Message, MessageActionRow, MessageComponentInteraction, MessageSelectOptionData, SelectMenuInteraction, Util } from "discord.js";
-import { FullUser, User } from "twitter-d";
+import { FullUser, Status, User } from "twitter-d";
 import Tweetcord from "../components/Client";
 import Command from "../components/Command";
 
@@ -12,59 +12,108 @@ export default class Search extends Command {
     public async reply(interaction: CommandInteraction): Promise<Message | any> {
         await interaction.defer()
         if (interaction.options.get("username")) {
-            try {
-                const data: FullUser[] = await this.bot.twitter.get("users/search", {
-                    q: interaction.options.get("username")?.value
+            const data: FullUser[] = await this.bot.twitter.get("users/search", {
+                q: interaction.options.get("username")?.value
+            })
+            if (data.length === 0) return interaction.reply({ content: "No results found", ephemeral: true })
+            const users = data.slice(0, 10)
+            const options: MessageSelectOptionData[] = users.map((u, i) => {
+                return Object.assign({}, {
+                    label: u.screen_name,
+                    description: u.description?.length === 0 ? "No description" : (u.description?.length! > 50 ? u.description?.substring(0, 49) + "\u2026" : u.description)!,
+                    value: (++i).toString()
                 })
-                if (data.length === 0) return interaction.reply({ content: "No results found", ephemeral: true })
-                const users = data.slice(0, 10)
-                const options: MessageSelectOptionData[] = users.map((u, i) => {
-                    return Object.assign({}, {
-                        label: u.screen_name,
-                        description: u.description?.length === 0 ? "No description" : (u.description?.length! > 50 ? u.description?.substring(0, 49) + "\u2026" : u.description)!,
-                        value: (++i).toString()
-                    })
+            })
+            const row = new MessageActionRow().addComponents({
+                "type": "SELECT_MENU",
+                "placeholder": "Click me!",
+                "customId": "users",
+                options
+            })
+            await interaction.followUp({ content: "Select user below", components: [row], ephemeral: true })
+            const filter = (i: SelectMenuInteraction) => i.user.id === interaction?.user.id
+            const collector = interaction.channel?.createMessageComponentCollector({ filter, time: 60e3 })
+            collector?.on("collect", (i: SelectMenuInteraction) => {
+                if (i.customId === "users") {
+                    i.deferUpdate()
+                    this.user(i, users[Number(i.values[0]) - 1].screen_name)
+                }
+            })
+            collector?.on("end", (collected) => {
+                collected.first()?.editReply({
+                    content: "⏰ Time's up", components: [
+                        new MessageActionRow().addComponents(
+                            {
+                                type: "BUTTON",
+                                style: "LINK",
+                                url: "https://tweetcord.xyz",
+                                label: "Visit our website!"
+                            },
+                            {
+                                type: "BUTTON",
+                                style: "LINK",
+                                url: "https://discord.gg/ZWfpZuw4mn",
+                                label: "Join support server!"
+                            }
+                        )
+                    ]
                 })
-                const row = new MessageActionRow().addComponents({
-                    "type": "SELECT_MENU",
-                    "placeholder": "Click me!",
-                    "customId": "users",
-                    options
+            })
+
+        } else if (interaction.options.get("text")) {
+            const data = await this.bot.twitter.get("search/tweets", {
+                q: interaction.options.get("text")?.value
+            })
+            if (data.statuses.length === 0) return interaction.reply({ content: "No results found", ephemeral: true })
+            const statuses: Status[] = data.statuses
+            const tweets = statuses.slice(0, 10)
+            const options: MessageSelectOptionData[] = tweets.map((u, i) => {
+                return Object.assign({}, {
+                    label: (u.user as FullUser).screen_name,
+                    description: u.text?.length === 0 ? "No description" : (u.text?.length! > 50 ? u.text?.substring(0, 49) + "\u2026" : u.text)!,
+                    value: (++i).toString()
                 })
-                await interaction.followUp({ content: "Select user below", components: [row], ephemeral: true })
-                const filter = (i: SelectMenuInteraction) => i.user.id === interaction?.user.id
-                const collector = interaction.channel?.createMessageComponentCollector({ filter, time: 30e3 })
-                collector?.on("collect", (i: SelectMenuInteraction) => {
-                    if (i.customId === "users") {
-                        i.deferUpdate()
-                        this.user(i, users[Number(i.values[0]) - 1].screen_name)
-                    }
-                })
-                collector?.on("end", (collected) => {
-                    collected.first()?.editReply({
-                        content: "⏰ Time's up", components: [
-                            new MessageActionRow().addComponents(
-                                {
-                                    type: "BUTTON",
-                                    style: "LINK",
-                                    url: "https://tweetcord.xyz",
-                                    label: "Visit our website!"
-                                },
-                                {
-                                    type: "BUTTON",
-                                    style: "LINK",
-                                    url: "https://discord.gg/ZWfpZuw4mn",
-                                    label: "Join support server!"
-                                }
-                            )
-                        ]
-                    })
-                })
-            } catch (err) {
-                console.error(err)
-            }
+            })
+            const row = new MessageActionRow().addComponents({
+                "type": "SELECT_MENU",
+                "placeholder": "Click me!",
+                "customId": "tweets",
+                options
+            })
+            await interaction.editReply({ content: "Select user below", components: [row] })
+            
+              const filter = (i: SelectMenuInteraction) => i.user.id === interaction?.user.id
+              const collector = interaction.channel?.createMessageComponentCollector({ filter, time: 60e3 })
+              
+              collector?.on("collect", (i: SelectMenuInteraction) => {
+                  if (i.customId === "tweets") {
+                      i.deferUpdate()
+                      const tweet = tweets[Number(i.values[0]) - 1]
+                      i.followUp({ content: `https://twitter.com/i/web/status/${tweet.id_str}`})
+                  }
+              })
+              collector?.on("end", (collected) => {
+                  collected.first()?.editReply({
+                      content: "⏰ Time's up", components: [
+                          new MessageActionRow().addComponents(
+                              {
+                                  type: "BUTTON",
+                                  style: "LINK",
+                                  url: "https://tweetcord.xyz",
+                                  label: "Visit our website!"
+                              },
+                              {
+                                  type: "BUTTON",
+                                  style: "LINK",
+                                  url: "https://discord.gg/ZWfpZuw4mn",
+                                  label: "Join support server!"
+                              }
+                          )
+                      ]
+                  })
+              })
+  
         }
-        // TODO: Add tweet search support
     }
     private async user(i: SelectMenuInteraction, username: string) {
         let data = await this.bot.twitter.get("users/lookup", {
