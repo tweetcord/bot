8502a6b6d96c92bfd08001190c6e95e576d0b016
Neriman Memmedli <neriman.memmedli001@gmail.com>
2021-08-13 12:13:17 +0400

    remove kurasuta

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 import Tweetcord from "./components/Client";
 import { config } from "dotenv";
-/*
-import { BaseCluster } from "kurasuta";
-export default class extends BaseCluster {
-    declare public client: Tweetcord
-    launch() {
-        this.client.init()
-    }
-};
-*/
-
 config()
 const client = new Tweetcord()
 client.init()
