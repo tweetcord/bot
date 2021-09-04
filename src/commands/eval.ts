cd811136f1b2b7b7ae7c87f1850055f4077e3f8b
Neriman Memmedli <neriman.memmedli001@gmail.com>
2021-09-04 14:03:19 +0400

    handle >2k limit

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, Formatters } from "discord.js";
+import { CommandInteraction, Formatters, Util } from "discord.js";
 import { inspect } from "util";
 import Tweetcord from "../components/Client";
 import Command from "../components/Command";
@@ -16,13 +16,19 @@ export default class Eval extends Command {
             const asynchr = code.includes('return') || code.includes('await');
             let output = await eval(asynchr ? `(async()=>{${code}})();` : code)
             if (typeof output !== "string") output = inspect(output, { depth: 0 })
+            if (output.length >= 2001) {
+                let array = Util.splitMessage(output);
+                return array.forEach(async a => {
+                    await interaction.followUp({ content: Formatters.blockQuote(Formatters.codeBlock("js", a.replace(new RegExp(this.bot.token!, 'gi'), "[TOKEN]"))) })
+                })
+            }
             return interaction.editReply({
-                content: Formatters.codeBlock("js", output.replace(new RegExp(this.bot.token!, 'gi'), "[TOKEN]"))
+                content: Formatters.blockQuote(Formatters.codeBlock("js", output.replace(new RegExp(this.bot.token!, 'gi'), "[TOKEN]"))),
             })
         } catch (err: any) {
             console.error("Eval command error:", err)
             return interaction.editReply({
-                content: Formatters.codeBlock("js", err.message.replace(new RegExp(this.bot.token!, 'gi'), "[TOKEN]"))
+                content: Formatters.blockQuote(Formatters.codeBlock("js", err.message.replace(new RegExp(this.bot.token!, 'gi'), "[TOKEN]")))
             })
         }
     }
