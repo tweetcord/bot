fd560baedfcf0448415e595439c762746eca1197
Neriman Memmedli <neriman.memmedli001@gmail.com>
2021-09-05 17:39:17 +0400

    fix type

--- @types/discord.js/index.d.ts
+++ @types/discord.js/index.d.ts
@@ -1,12 +1,12 @@
-import { Client, Collection } from "discord.js";
-import Command from "../components/Command";
-import Twitter from "twitter-lite";
 import { PrismaClient } from "@prisma/client";
+import { Collection } from "discord.js";
+import { TwitterApiReadOnly } from "twitter-api-v2";
+import Command from "../components/Command";
 
 declare module "discord.js" {
   export interface Client {
     readonly commands: Collection<string, Command>;
-    twitter: Twitter;
+    twitter: TwitterApiReadOnly;
     prisma: PrismaClient;
   }
 }
