f866dd86ae72e7fc85f2496d8b7bb14feb7d3ca0
WoxeOfficial <erenwoxedemir@gmail.com>
2021-11-13 10:27:03 +0300

    add more try{}

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -6,80 +6,84 @@ import SelectMenu from "../components/SelectMenu";
 import { checkNSFW, getButtons, iDefer, iFollowUp } from "../utils/functions";
 
 export default class Search extends Command {
-    public data() {
-        return new SlashCommandBuilder()
-            .setName("search")
-            .setDescription("Allows you to search tweet/user on Twitter")
-            .addSubcommand((slash) =>
-                slash
-                    .setName("tweet")
-                    .setDescription("Tweet search")
-                    .addStringOption((option) => option.setName("text").setDescription("Text to search that tweets contain").setRequired(true))
-            )
-            .addSubcommand((slash) =>
-                slash
-                    .setName("user")
-                    .setDescription("User search")
-                    .addStringOption((option) => option.setName("username").setDescription("Username to search").setRequired(true))
-            );
-    }
-    public async run(interaction: CommandInteraction): Promise<Message | void> {
-        await iDefer(interaction);
-        if (!checkNSFW(interaction)) return;
-        const subcommand = interaction.options.getSubcommand(true);
-        if (subcommand === "tweet") {
-            const { data } = await interaction.client.twitter.v2.search(interaction.options.getString("text", true), {
-                max_results: 100,
-            });
-            const answers: InteractionReplyOptions[] = [];
-            const tweets = data.data;
-            let id = Date.now().toString();
-            let [TweetsFirstRow, TweetsRow, TweetsLastRow] = getButtons(id);
+  public data() {
+    return new SlashCommandBuilder()
+      .setName("search")
+      .setDescription("Allows you to search tweet/user on Twitter")
+      .addSubcommand((slash) =>
+        slash
+          .setName("tweet")
+          .setDescription("Tweet search")
+          .addStringOption((option) => option.setName("text").setDescription("Text to search that tweets contain").setRequired(true))
+      )
+      .addSubcommand((slash) =>
+        slash
+          .setName("user")
+          .setDescription("User search")
+          .addStringOption((option) => option.setName("username").setDescription("Username to search").setRequired(true))
+      );
+  }
+  public async run(interaction: CommandInteraction): Promise<Message | void> {
+    try {
+      await iDefer(interaction);
+      if (!checkNSFW(interaction)) return;
+      const subcommand = interaction.options.getSubcommand(true);
+      if (subcommand === "tweet") {
+        const { data } = await interaction.client.twitter.v2.search(interaction.options.getString("text", true), {
+          max_results: 100,
+        });
+        const answers: InteractionReplyOptions[] = [];
+        const tweets = data.data;
+        let id = Date.now().toString();
+        let [TweetsFirstRow, TweetsRow, TweetsLastRow] = getButtons(id);
 
-            for (let i = 0; i < tweets?.length; i++) {
-                const url = `https://twitter.com/i/web/status/${tweets.at(i)?.id}`;
-                answers?.push({
-                    content: `**(${i + 1}/${tweets?.length})** ${url}`,
-                    components: answers.length === 0 ? [TweetsFirstRow] : tweets?.length === i + 1 ? [TweetsLastRow] : [TweetsRow],
-                });
-            }
-            if (!tweets) {
-                await iFollowUp(interaction, {
-                    content: "No results found.",
-                    ephemeral: true,
-                });
-                return;
-            }
-            await iFollowUp(interaction, {
-                content: `**(1/${tweets.length})** https://twitter.com/i/web/status/${tweets.at(0)?.id}`,
-                components: [TweetsFirstRow],
-                ephemeral: true,
-            });
-            const menu = new ButtonMenu(answers, id);
-            return menu.start({ interaction });
-        } else if (subcommand === "user") {
-            const { data } = await interaction.client.twitter.v1.searchUsers(interaction.options.getString("username", true));
-            const options: MessageSelectOptionData[] = data.slice(0, 25).map((u, i) => {
-                return Object.assign(
-                    {},
-                    {
-                        label: u.screen_name,
-                        // Description 100den büyükse 99dan kesip 3 nokta unicode'u ekliyor
-                        // https://discord.com/developers/docs/interactions/message-components#select-menu-object
-                        description: u.description?.length === 0 ? "No description" : (u.description?.length! > 100 ? u.description?.substring(0, 99) + "\u2026" : u.description)!,
-                        value: (++i).toString(),
-                    }
-                );
-            });
-            if (options.length === 0) {
-                await iFollowUp(interaction, {
-                    content: "No results found.",
-                    ephemeral: true,
-                });
-                return;
-            }
-            const menu = new SelectMenu(options);
-            return menu.start({ interaction, data });
+        for (let i = 0; i < tweets?.length; i++) {
+          const url = `https://twitter.com/i/web/status/${tweets.at(i)?.id}`;
+          answers?.push({
+            content: `**(${i + 1}/${tweets?.length})** ${url}`,
+            components: answers.length === 0 ? [TweetsFirstRow] : tweets?.length === i + 1 ? [TweetsLastRow] : [TweetsRow],
+          });
         }
+        if (!tweets) {
+          await iFollowUp(interaction, {
+            content: "No results found.",
+            ephemeral: true,
+          });
+          return;
+        }
+        await iFollowUp(interaction, {
+          content: `**(1/${tweets.length})** https://twitter.com/i/web/status/${tweets.at(0)?.id}`,
+          components: [TweetsFirstRow],
+          ephemeral: true,
+        });
+        const menu = new ButtonMenu(answers, id);
+        return menu.start({ interaction });
+      } else if (subcommand === "user") {
+        const { data } = await interaction.client.twitter.v1.searchUsers(interaction.options.getString("username", true));
+        const options: MessageSelectOptionData[] = data.slice(0, 25).map((u, i) => {
+          return Object.assign(
+            {},
+            {
+              label: u.screen_name,
+              // Description 100den büyükse 99dan kesip 3 nokta unicode'u ekliyor
+              // https://discord.com/developers/docs/interactions/message-components#select-menu-object
+              description: u.description?.length === 0 ? "No description" : (u.description?.length! > 100 ? u.description?.substring(0, 99) + "\u2026" : u.description)!,
+              value: (++i).toString(),
+            }
+          );
+        });
+        if (options.length === 0) {
+          await iFollowUp(interaction, {
+            content: "No results found.",
+            ephemeral: true,
+          });
+          return;
+        }
+        const menu = new SelectMenu(options);
+        return menu.start({ interaction, data });
+      }
+    } catch (e) {
+      console.log(e);
     }
+  }
 }
--- src/commands/tweets.ts
+++ src/commands/tweets.ts
@@ -17,44 +17,48 @@ export default class Trend extends Command {
   }
 
   public async run(interaction: CommandInteraction): Promise<Message | void> {
-    !interaction.deferred && (await iDefer(interaction));
-    if (!checkNSFW(interaction)) return;
-    let username = interaction.options.getString("username", true);
-    if (username.length >= 15 && username.includes(" ")) {
-      iFollowUp(interaction, { content: emojis.f + "The length of the username can't exceed 15 characters or include any spaces." });
-      return;
-    }
     try {
-      let { data: user } = await interaction.client.twitter.v2.userByUsername(username);
-      if (!user) {
-        iFollowUp(interaction, { content: emojis.f + "Can't find any users with named **" + username + "**" });
+      !interaction.deferred && (await iDefer(interaction));
+      if (!checkNSFW(interaction)) return;
+      let username = interaction.options.getString("username", true);
+      if (username.length >= 15 && username.includes(" ")) {
+        iFollowUp(interaction, { content: emojis.f + "The length of the username can't exceed 15 characters or include any spaces." });
         return;
       }
-      const exclude: TypeOrArrayOf<"replies" | "retweets"> | undefined = [];
-      if (interaction.options.getBoolean("hide_replies")) exclude.push("replies");
-      if (interaction.options.getBoolean("hide_retweets")) exclude.push("retweets");
-      const data = await interaction.client.twitter.v2.userTimeline(user.id, { exclude });
-      if (data?.tweets?.length === 0) return iReply(interaction, { content: "No tweets found", ephemeral: true });
-      const answers: InteractionReplyOptions[] = [];
-      const tweets = data?.tweets.slice(0, data?.tweets.length > 10 ? 10 : data.tweets.length);
-      let id = Date.now().toString();
-      let [TweetsFirstRow, TweetsRow, TweetsLastRow] = getButtons(id);
-      for (let i = 0; i < tweets?.length; i++) {
-        const url = `https://twitter.com/i/web/status/${tweets.at(i)?.id}`;
-        answers?.push({
-          content: `**(${i + 1}/${tweets.length})** ${url}`,
-          components: answers.length === 0 ? [TweetsFirstRow] : tweets.length === i + 1 ? [TweetsLastRow] : [TweetsRow],
+      try {
+        let { data: user } = await interaction.client.twitter.v2.userByUsername(username);
+        if (!user) {
+          iFollowUp(interaction, { content: emojis.f + "Can't find any users with named **" + username + "**" });
+          return;
+        }
+        const exclude: TypeOrArrayOf<"replies" | "retweets"> | undefined = [];
+        if (interaction.options.getBoolean("hide_replies")) exclude.push("replies");
+        if (interaction.options.getBoolean("hide_retweets")) exclude.push("retweets");
+        const data = await interaction.client.twitter.v2.userTimeline(user.id, { exclude });
+        if (data?.tweets?.length === 0) return iReply(interaction, { content: "No tweets found", ephemeral: true });
+        const answers: InteractionReplyOptions[] = [];
+        const tweets = data?.tweets.slice(0, data?.tweets.length > 10 ? 10 : data.tweets.length);
+        let id = Date.now().toString();
+        let [TweetsFirstRow, TweetsRow, TweetsLastRow] = getButtons(id);
+        for (let i = 0; i < tweets?.length; i++) {
+          const url = `https://twitter.com/i/web/status/${tweets.at(i)?.id}`;
+          answers?.push({
+            content: `**(${i + 1}/${tweets.length})** ${url}`,
+            components: answers.length === 0 ? [TweetsFirstRow] : tweets.length === i + 1 ? [TweetsLastRow] : [TweetsRow],
+          });
+        }
+        await iFollowUp(interaction, {
+          content: `**(1/${tweets.length})** https://twitter.com/i/web/status/${tweets.at(0)?.id}`,
+          components: [TweetsFirstRow],
         });
-      }
-      await iFollowUp(interaction, {
-        content: `**(1/${tweets.length})** https://twitter.com/i/web/status/${tweets.at(0)?.id}`,
-        components: [TweetsFirstRow],
-      });
 
-      const menu = new ButtonMenu(answers, id);
-      return menu.start({ interaction });
+        const menu = new ButtonMenu(answers, id);
+        return menu.start({ interaction });
+      } catch (e) {
+        await iFollowUp(interaction, { content: emojis.f + "Can't find any users with named **" + username + "**" });
+      }
     } catch (e) {
-      await iFollowUp(interaction, { content: emojis.f + "Can't find any users with named **" + username + "**" });
+      console.log(e);
     }
   }
 }
